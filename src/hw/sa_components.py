e6c18d3e9e3a1b7518b421c143634745380c537b
Jeronimo <jeronimopenha@gmail.com>
2022-06-06 19:38:40 -0300

    .

--- src/hw/sa_components.py
+++ src/hw/sa_components.py
@@ -11,8 +11,10 @@ class SAComponents:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self):
+    def __init__(self, n_cells: int = 16, n_neighbors: int = 5, align_bits: int = 8):
         self.cache = {}
+        self.n_cells = n_cells
+        self.align_bits = align_bits
 
     def create_random_generator_11b(self) -> Module:
         # LFSR random generator based on Fibonacci's LFSR
@@ -107,6 +109,58 @@ class SAComponents:
         self.cache[name] = m
         return m
 
+    def create_graph_memory(self, n_cells: int, n_neighbors: int) -> Module:
+        name = 'graph_memory'
+        if name in self.cache.keys():
+            return self.cache[name]
+
+        m = Module(name)
+
+        bits_width = ceil(log2(n_cells))
+        bits_word = ceil((ceil(log2(n_cells)) + 1) /
+                         self.align_bits) * self.align_bits * n_neighbors
+
+        clk = m.Input('clk')
+
+        rd = m.Input('rd')
+        node1 = m.Input('node1', bits_width)
+        neighbors1_out = m.OutputReg('neighbors1_out', bits_word)
+
+        node2 = m.Input('node2', bits_width)
+        neighbors2_out = m.OutputReg(
+            'neighbors2_out', bits_word)
+
+        wr = m.Input('wr')
+        wr_addr = m.Input('wr_addr', bits_width)
+        data_in = m.Input('data_in', bits_word)
+
+        mem_neighbors = m.Reg('mem_neighbors', bits_word)
+
+        m.Always(Posedge(clk))(
+            If(wr)(
+                mem_neighbors[wr_addr](data_in)
+            ),
+            If(rd)(
+                neighbors1_out(mem_neighbors[node1]),
+                neighbors1_out(mem_neighbors[node2]),
+            )
+        )
+
+        m.EmbeddedCode('//synthesis translate_off')
+
+        i = m.Integer('i')
+        m.Initial(
+            neighbors1_out(0),
+            neighbors2_out(0),
+            For(i(0), i < Power(2, bits_width), i.inc())(
+                mem_neighbors[i](0),
+            ),
+            #Systask('readmemh', init_file, mem_data)
+        )
+        m.EmbeddedCode('//synthesis translate_on')
+        self.cache[name] = m
+        return m
+
     def create_manhatan_dist_calc(self, n_cells: int) -> Module:
         name = 'manhatan_dist_calc'
         if name in self.cache.keys():
@@ -114,7 +168,7 @@ class SAComponents:
 
         m = Module(name)
 
-        bits = ceil(log2(n_cells))
+        bits = ceil(log2(n_cells))/8
 
         clk = m.Input('clk')
         cell1 = m.Input('cell1', bits)
@@ -128,11 +182,12 @@ class SAComponents:
         c1 = m.Wire('c1', bits)
         c2 = m.Wire('c2', bits)
 
-        l1.assign(Cat(Int(0, bits//2, 2), cell1[0:bits//2]))
-        l2.assign(Cat(Int(0, bits//2, 2), cell2[0:bits//2]))
-        c1.assign(Cat(Int(0, bits//2, 2), cell1[bits//2:cell1.width]))
-        c2.assign(Cat(Int(0, bits//2, 2), cell2[bits//2:cell2.width]))
+        l1.assign(Cat(Int(0, int(bits/2), 2), cell1[0:int(bits/2)]))
+        l2.assign(Cat(Int(0, int(bits/2), 2), cell2[0:int(bits/2)]))
+        c1.assign(Cat(Int(0, int(bits/2), 2), cell1[int(bits/2):cell1.width]))
+        c2.assign(Cat(Int(0, int(bits/2), 2), cell2[int(bits/2):cell2.width]))
 
+        # TODO tentar otimizar para 1 clock
         m.Always(Posedge(clk))(
             parc_1(Mux(l1 < l2, l2-l1, l1-l2)),
             parc_2(Mux(c1 < c2, c2-c1, c1-c2)),
@@ -143,10 +198,20 @@ class SAComponents:
         self.cache[name] = m
         return m
 
+    def create_sa_thread(self, n_cells: int, n_neighbors: int) -> Module:
+        name = 'sa_thread'
+        if name in self.cache.keys():
+            return self.cache[name]
 
-    def create_sa_thread(self)->Module:
-        pass
+        m = Module(name)
+        clk = m.Input('clk')
+        rst = m.Input('rst')
+
+        self.cache[name] = m
+        return m
+
 
 comp = SAComponents()
 
 comp.create_manhatan_dist_calc(16).to_verilog("manhatan.v")
+#comp.create_graph_memory(16, 5).to_verilog("graph_memory.v")
